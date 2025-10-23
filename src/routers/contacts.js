06c554775abaa702a57fa03b38df728695bc6925
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-10-24 00:25:07 +0200

    fix 2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -5,7 +5,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlwares/validateBody.js';
 import { isValidId } from '../middlwares/isValidId.js';
 import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
-import authenticate from '../middlewares/authenticate.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
