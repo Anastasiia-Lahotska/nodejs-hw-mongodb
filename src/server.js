8330ad17505247f317eb084658a1f34a94018024
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-10-26 22:29:49 +0100

    step 4-7

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-/*import { getContactsController, getContactByIdController } from './controllers/contacts.js';*/
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
 import { errorHandler } from './middlwares/errorHandler.js';
 import { notFoundHandler } from './middlwares/notFoundHandler.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 export const setupServer = () => {
   const app = express();
@@ -15,21 +18,22 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(pino());
 
-  /*app.get('/contacts', getContactsController);
-  app.get('/contacts/:contactId', getContactByIdController);*/
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const swaggerPath = path.resolve(__dirname, '../docs/openapi.yaml');
+  const swaggerDocument = YAML.load(swaggerPath);
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
   app.use(notFoundHandler);
   app.use(errorHandler)
 
-  /*app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-  });*/
-
   const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
   });
 
   return app;
