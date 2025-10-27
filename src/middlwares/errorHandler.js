134e74c666ee840514140bf6170ba8674f50dd56
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-10-27 20:52:31 +0100

    fix post/auth+login

--- src/middlwares/errorHandler.js
+++ src/middlwares/errorHandler.js
@@ -1,7 +1,9 @@
-export const errorHandler = (err, req, res) => {
+export const errorHandler = (err, req, res, next) => {
+  console.error('Error:', err.message);
+
   res.status(err.status || 500).json({
     status: err.status || 500,
     message: err.message || 'Something went wrong',
-    data: err.data || null,
+    data: null,
   });
 };
