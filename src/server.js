5cce5a9cf32dbfcb09d110cd89086b9968217d33
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-10-29 21:19:37 +0100

    Fix swagger docs: remove theme and add contact schema

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import pino from 'pino-http';
 import contactsRouter from './routers/contacts.js';
@@ -16,6 +17,7 @@ export const setupServer = () => {
 
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
   app.use(pino());
 
   const __filename = fileURLToPath(import.meta.url);
