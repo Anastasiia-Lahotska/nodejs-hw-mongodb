1d136497c992c261ebf047a400d5c8f547c27926
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-10-31 15:04:01 +0100

    fix params

--- src/server.js
+++ src/server.js
@@ -3,16 +3,18 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import pino from 'pino-http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import swaggerUi from 'swagger-ui-express';
+
 import { UPLOAD_DIR } from './constants/contacts.js';
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
 import { errorHandler } from './middlwares/errorHandler.js';
 import { notFoundHandler } from './middlwares/notFoundHandler.js';
-import { swaggerDocs } from './middlwares/swaggerDocs.js';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+
+const swaggerDocument = JSON.parse(fs.readFileSync('./docs/swagger.json', 'utf8'));
 
 export const setupServer = () => {
   const app = express();
@@ -22,24 +24,16 @@ export const setupServer = () => {
   app.use(cookieParser());
   app.use(pino());
   app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
-
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  const swaggerPath = path.resolve(__dirname, '../docs/openapi.yaml');
-  const swaggerDocument = YAML.load(swaggerPath);
-
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
   app.use(notFoundHandler);
-  app.use(errorHandler)
+  app.use(errorHandler);
 
-  const PORT = process.env.PORT || 3000;
-
+ const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
+    console.log(`✅ Server is running on port ${PORT}`);
+    console.log(`📘 Swagger Docs available at http://localhost:${PORT}/api-docs`);
   });
 
   return app;
